51f1a5bd548f9bf34392d933a27fbd8f7bfcab9c
Shanupal <164392576+Shanupal@users.noreply.github.com>
2025-01-28 21:41:00 +0530

    Update hotel.py

--- hotel.py
+++ hotel.py
@@ -1,14 +1,14 @@
 menu = {
     'pizza':40,
-    'pasta' :50,
-    'burger' : 60,
-    'salad' :70,
-    'coffee':80,
+    'pasta' :10,
+    'burger' : 30,
+    'salad' :15,
+    'coffee':100,
     'momo' :20,
 }
 
 print("welcome to sagar restaurant")
-print("pizza: $40\n pasta:$10\n burger:$30\n salad:$15\n coffee:$100")
+print("pizza: $40\n pasta:$10\n burger:$30\n salad:$15\n coffee:$100\n momo:$20")
 
 
 order_total = 0
@@ -21,10 +21,17 @@ else:
     print(f"ordered item {item_1} is not avaialable you")
 
 another_order = input("Do you want to add another item? (yes/no)")
-if another_order in menu:
+
+if another_order == "yes":
+    item_2 = input("name of item you want to order:")
     order_total += menu[item_2]
     print(f"item {item_2} has been added to order")
 else:
     print("ordered item {iten_2} is not avaialable")
+# if another_order in menu:
+#     order_total += menu[item_1]
+#     print(f"item {item_1} has been added to order")
+# else:
+#     print("ordered item {iten_2} is not avaialable")
     
 print(f"the total amount of itema to pay is {order_total}")
